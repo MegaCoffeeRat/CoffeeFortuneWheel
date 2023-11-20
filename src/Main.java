d2db81050de1cb00beb5f1085947f33bd5c1548f
MegaCoffeeRat <fedorenko.css@gmail.com>
2023-11-20 14:25:06 -0500

    new strat

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ class Main {
 
     public static void main(String[] args)
     {
+        System.out.println();
 
         }
 
--- src/RandomizerWheel.java
+++ src/RandomizerWheel.java
@@ -1,16 +1,61 @@
 
 public class RandomizerWheel {
-String[] size
+String[] size = {"Large", "Medium", "Small"};
+String[] roast = {"Light","Medium","Dark"};
+String[] syrup = {"Chocolate", "Vanilla", "Caramel"};
+String[] temp = {"Iced", "Warm", "Hot"};
+
+
+String Size = "";
+String Roast = "";
+String Syrup = "";
+String Tmep = "";
+
 
     public static void printLn(String txt)
     {
         System.out.println(txt);
     }
+
+    public static boolean Syrup()
+    {
+        int rnd = (int)(Math.random()*2);
+        if(rnd <= 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean Honey()
+    {
+        int rnd = (int)(Math.random()*2);
+        if(rnd <= 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
     public  void randomizer(String whatTo)
     {
-
         if(whatTo.equals("size"))
         {
+            int random = (int)(Math.random()*3);
+            if(random == 1)
+            {
+
+            }
+            else if(random == 2)
+            {
+
+            }
+            else
+            {
+
+            }
+
 
         }
 
